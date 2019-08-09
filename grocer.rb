9114c9430344f73f9da553ff7c05883267bca782
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 20:52:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ end
 
 def apply_clearance(cart)
   cart.keys.each do |item|
-    if cart[item]
+    if cart[item][:clearance]
       cart[item][:price] = (cart[item][:price] * 0.80).round(2)
     end  
   end  
