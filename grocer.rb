5529669f06d8d307f2767e9befab67ba2586ea49
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 19:28:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,7 @@
 def consolidate_cart(cart)
   new = {}
   cart.each { |index|
-    if new.key.include?(cart[index])
+    if new.key.include?(cart[index.key])
       :count += 1
     else
       new.push(cart[index].key)
