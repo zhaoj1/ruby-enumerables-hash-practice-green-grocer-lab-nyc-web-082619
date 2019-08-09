e6336e6abbc468a66d74f73c31dd5befbe7c8157
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 19:08:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,17 @@
 def consolidate_cart(cart)
   
+  
+  
   [
   {"AVOCADO" => {:price => 3.00, :clearance => true }},
   {"AVOCADO" => {:price => 3.00, :clearance => true }},
   {"KALE"    => {:price => 3.00, :clearance => false}}
 ]
  
- 
-  
+ {
+  "AVOCADO" => {:price => 3.00, :clearance => true, :count => 2},
+  "KALE"    => {:price => 3.00, :clearance => false, :count => 1}
+}
   
 end
 
