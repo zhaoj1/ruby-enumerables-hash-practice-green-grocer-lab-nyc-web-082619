66827edf4678c9cb94c9d8391b72247fe882efb7
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 20:31:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ def apply_coupons(cart, coupons)
     if cart.keys.include?(coupon[:item])
       coupon_item = "#{coupon[:item} W/COUPON"
         if cart[coupon_item]
-          cart[coupon_item][:count] += coupon 
+          cart[coupon_item][:count] += coupon[:num] 
   }
 end
 
