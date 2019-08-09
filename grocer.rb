36f5aab350c8be8fd9a207c0c3eb46e891c25e85
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-09 19:39:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,6 +13,21 @@ def consolidate_cart(cart)
   new
 end
 
+def consolidate_cart(cart)
+  new_hash = {}
+  cart.each do |item|
+    if new_hash[item.keys[0]]
+      new_hash[item.keys[0]][:count] += 1
+    else
+      new_hash[item.keys[0]] = {
+        count: 1,
+        price: item.values[0][:price],
+        clearance: item.values[0][:clearance]
+      }
+    end
+  end
+  new_hash
+end
 
 
 
